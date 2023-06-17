7fc2d99ca491b16272a6a50669935c6e99ff367e
Naphtali Gilead <naphtul@gmail.com>
2023-06-17 10:25:38 -0400

    Initial commit

---
+++ MyQ.py
@@ -0,0 +1,39 @@
+import asyncio
+import os
+
+import pymyq
+from aiohttp import ClientSession
+
+
+class MyQ:
+    def __init__(self):
+        self.user = os.environ.get('MYQ_USER')
+        self.pwd = os.environ.get('MYQ_PASS')
+        self.legend = {}
+        self.get_garage_doors()
+        self.myq = None
+        self.web_session = ClientSession()
+
+    async def get_garage_doors(self) -> dict:
+        self.myq = await pymyq.login(self.user, self.pwd, self.web_session)
+        garage_doors = self.myq.covers
+        for id, door in garage_doors.items():
+            self.legend[door.name] = dict(id=id, state=door.state)
+        return self.legend
+
+    async def open_garage_door(self, name: str) -> bool:
+        id = self.legend.get(name).get('id')
+        state = await self.myq.covers.get(id).open()
+        return state
+
+    async def close_garage_door(self, name: str) -> bool:
+        id = self.legend.get(name).get('id')
+        state = await self.myq.covers.get(id).close()
+        return state
+
+
+if __name__ == '__main__':
+    my_q = MyQ()
+    asyncio.get_event_loop().run_until_complete(my_q.get_garage_doors())
+    asyncio.get_event_loop().run_until_complete(my_q.open_garage_door('Grass'))
+    asyncio.get_event_loop().run_until_complete(my_q.close_garage_door('Grass'))
