555ee0d5cd69b8e8c225340278f23b5c80527e10
Naphtali Gilead <naphtul@gmail.com>
2023-06-23 15:57:55 -0400

    Making the class even more synchronous

--- src/myq/MyQ.py
+++ src/myq/MyQ.py
@@ -11,31 +11,37 @@ class MyQ:
         self.pwd = pwd if pwd else os.environ.get('MYQ_PASS')
         if not (self.user and self.pwd):
             raise ValueError('Username and Password params are not properly defined.')
-        self.legend = {}
-        self.get_garage_doors()
         self.myq = None
+        self.legend = {}
         self.web_session = ClientSession()
+        self.event_loop = asyncio.get_event_loop()
+        self.refresh_garage_doors()
 
-    async def get_garage_doors(self) -> dict:
-        self.myq = await pymyq.login(self.user, self.pwd, self.web_session)
+    def refresh_garage_doors(self) -> dict:
+        self.myq = self.event_loop.run_until_complete(pymyq.login(self.user, self.pwd, self.web_session))
         garage_doors = self.myq.covers
         for id, door in garage_doors.items():
             self.legend[door.name] = dict(id=id, state=door.state)
         return self.legend
 
-    async def open_garage_door(self, name: str) -> bool:
-        id = self.legend.get(name).get('id')
-        state = await self.myq.covers.get(id).open()
-        return state
+    def get_garage_doors(self) -> dict:
+        if not self.legend:
+            return self.refresh_garage_doors()
+        return self.legend
 
-    async def close_garage_door(self, name: str) -> bool:
+    def open_garage_door(self, name: str) -> bool:
         id = self.legend.get(name).get('id')
-        state = await self.myq.covers.get(id).close()
-        return state
+        res = self.event_loop.run_until_complete(self.myq.covers.get(id).open())
+        return True if res._state == 'PENDING' else False
+
+    def close_garage_door(self, name: str) -> bool:
+        id = self.legend.get(name).get('id')
+        res = self.event_loop.run_until_complete(self.myq.covers.get(id).close())
+        return True if res._state == 'PENDING' else False
 
 
 if __name__ == '__main__':
     my_q = MyQ('', '')
-    asyncio.get_event_loop().run_until_complete(my_q.get_garage_doors())
-    asyncio.get_event_loop().run_until_complete(my_q.open_garage_door('Grass'))
-    asyncio.get_event_loop().run_until_complete(my_q.close_garage_door('Grass'))
+    print(my_q.get_garage_doors())
+    my_q.open_garage_door('Grass')
+    my_q.close_garage_door('Grass')
